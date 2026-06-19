d4886e039c0ec64b01902b30b37b6281262887cc
Renan Silva <renansilvaytB@gmail.com>
2026-06-19 09:11:01 -0300

    feat: Operadores Lógicos

---
+++ Aula09/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Aula09;
+
+public class Programa
+{
+  public static void Main()
+  {
+    // Operadores Lógicos
+
+    Console.WriteLine("Operadores lógicos");
+    bool estaLogado = true;
+    bool possuiPermissaoAdmin = false;
+
+    Console.WriteLine("O usuário está logado? " + estaLogado);
+    Console.WriteLine("O usuário possui permissão de administrador? " + (estaLogado && possuiPermissaoAdmin));
+
+    if (estaLogado)
+    {
+      Console.WriteLine("O usuário tem acesso ao sistema.");
+      if (possuiPermissaoAdmin)
+      {
+        Console.WriteLine("O usuário tem acesso às funcionalidades de administrador.");
+      }
+      else
+      {
+        Console.WriteLine("O usuário não tem acesso às funcionalidades de administrador.");
+      }
+    }
+    else
+    {
+      Console.WriteLine("O usuário não tem acesso ao sistema.");
+    }
+
+    if (estaLogado || possuiPermissaoAdmin)
+    {
+      Console.WriteLine("O usuário tem acesso ao sistema ou é administrador.");
+    }
+    else
+    {
+      Console.WriteLine("O usuário não tem acesso ao sistema e não é administrador.");
+    }
+  }
+}
