51e8da6d06be291045be899dc4a18bc0d19e9161
Renan Silva <renansilvaytB@gmail.com>
2026-06-19 08:55:13 -0300

    feat: operadores de atribuição

---
+++ Aula07/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Aula07;
+
+public class Programa
+{
+    public static void Main()
+  {
+    // operadores de atribuição
+
+    // iniciar uma variavel
+    int x = 10;
+    Console.WriteLine($"Valor inicial de x: {x}");
+
+    // operador de atribuição simples
+    x = 20;
+    Console.WriteLine($"Valor de x após atribuição simples: {x}");
+
+    // operador de atribuição composto
+    x += 5; // equivalente a x = x + 5
+    Console.WriteLine($"Valor de x após atribuição composta (x += 5): {x}");
+
+    // operador de atribuição composto (incremento)
+    x++; // equivalente a x = x + 1
+    Console.WriteLine($"Valor de x após incremento (x++): {x}");
+  }
+}
