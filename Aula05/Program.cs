d4d60a3eef55d6731a52d7c45cb2e73386ef83c7
Renan Silva <renansilvaytB@gmail.com>
2026-06-19 08:44:52 -0300

    feat: operadores aritméticos

---
+++ Aula05/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Aula05;
+
+public class Programa
+{
+    public static void Main()
+  {
+    // operadores aritméticos
+    int a = 10;
+    int b = 5;
+    
+    Console.WriteLine($"Soma: {a} + {b} = {a + b}");
+    Console.WriteLine($"Subtração: {a} - {b} = {a - b}");
+    Console.WriteLine($"Multiplicação: {a} * {b} = {a * b}");
+    Console.WriteLine($"Divisão: {a} / {b} = {a / b}");
+    Console.WriteLine($"Módulo: {a} % {b} = {a % b}");
+  }
+}
