5730ac4d85dd8ac0770b19d23ce8deefcbc206d2
Renan Silva <renansilvaytB@gmail.com>
2026-06-19 08:50:54 -0300

    feat: condicionais if e else

---
+++ Aula06/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Aula06;
+
+public class Programa
+{
+    public static void Main()
+  {
+    // condicionais
+
+    Console.WriteLine("Digite sua idade:");
+    int idade = int.Parse(Console.ReadLine());
+
+    if (idade >= 18)
+    {
+      Console.WriteLine("Você é maior de idade.");
+    }
+    else
+    {
+      Console.WriteLine("Você é menor de idade.");
+    }
+  }
+}
