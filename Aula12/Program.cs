2e45ee85cd0a400d799cae05ab7d301163185ef8
Renan Silva <renansilvaytB@gmail.com>
2026-06-22 19:05:14 -0300

    feat: adicionar arquivos do projeto Aula12

---
+++ Aula12/Program.cs
@@ -0,0 +1,16 @@
+namespace Aula12;
+
+public class Programa
+{
+  public static void Main()
+  {
+    // while - contagem de 1 a 5
+    int contador = 1;
+
+    while (contador <= 5)
+    {
+      Console.WriteLine($"Contador: {contador}");
+      contador++;
+    }
+  }
+}
